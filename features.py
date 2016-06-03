24a826d5780606280f43cf1ea0db4b4c62af2311
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2016-06-02 22:42:23 -0700

    add h5 file parsing and gaussian features

--- features.py
+++ features.py
@@ -1,3 +1,7 @@
+STRIP_BINARY = {
+    'autocorr': False,
+    'gaussian': True,
+}
 
 FEATURES = {
     'autocorr':
@@ -19,6 +23,30 @@ FEATURES = {
             'rfi_checked',
             'rfi_found',
             'reserved'
+        ),
+    'gaussian':
+        (
+            'id',
+            'resultid',
+            'peak_power',
+            'mean_power',
+            'time',
+            'ra',
+            'dec',
+            'q_pix',
+            'freq',
+            'detection_freq',
+            'barycentric_freq',
+            'fft_len',
+            'chirp_rate',
+            'rfi_checked',
+            'rfi_found',
+            'reserved'
+            'sigma',
+            'chisqr',
+            'null_chisqr',
+            'score',
+            'max_power',
         )
 }
 
@@ -26,15 +54,13 @@ GOOD_FEATURES = {
     'autocorr':
         (
             'peak_power',
-            'mean_power',
-            'time',
-            'ra',
-            'dec',
+            #'time',
+            #'ra',
+            #'dec',
             'delay',
             'freq',
             'detection_freq',
             'barycentric_freq',
-            'fft_len',
             'chirp_rate',
         )
 }
